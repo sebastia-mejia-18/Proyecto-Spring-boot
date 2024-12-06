bc77d1ae5ef63051e6544d70e50b3ac672d89c43
sebastian mejia <juansebastianmejiaagudelo73@gmail.com>
2024-12-06 13:54:32 -0500

    proyecto de suguros en proceso

--- src/routes/routesPrincipal.jsx
+++ src/routes/routesPrincipal.jsx
@@ -1,18 +1,18 @@
-import Home from"../pages/Home";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Registros from "../pages/Registros";
 
-export let router=[
-    {
-        path:"/home",
-        element:<Home/>
-    },
-    {
-        path: "/Login",
-        element: <Login/>
-    },
-    {
-        path:"/Registros",
-        element:<Registros/>,
-    },
+export let router = [
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+  {
+    path: "/Registros",
+    element: <Registros />,
+  },
 ];
